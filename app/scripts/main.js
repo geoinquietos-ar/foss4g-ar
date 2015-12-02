9c51f888e92cc93bad6f594b5360d147d867dee7
Mauricio Miranda <maurimiranda@gmail.com>
2015-12-01 21:48:14 -0300

    Disable mouse wheel zoom

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,12 @@
-var map = L.map('map').setView([-34.57158, -58.43926], 14);
+var map = L.map('map', {
+  scrollWheelZoom: false,
+  center: [-34.57158, -58.43926],
+  zoom: 14
+});
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18
+  maxZoom: 18
 }).addTo(map);
 L.circle([-34.57158, -58.43926], 200, {
-    color: '#0073ae',
-    fillOpacity: 0.8
+  color: '#0073ae',
+  fillOpacity: 0.8
 }).addTo(map);
