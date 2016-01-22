2d9471fa884b995edae2eedd038e60a66869ab1b
zalitoar <zalitoar@gmail.com>
2016-01-22 18:43:06 -0300

    Agrego atribución del mapa

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,8 @@ var map = L.map('map', {
   zoom: 14
 });
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 18
+  maxZoom: 18,
+  attribution: '&copy; <a href="http://openstreetmap.org">Colaboradores de OpenStreetMap</a>'
 }).addTo(map);
 L.circle([-34.57158, -58.43926], 200, {
   color: '#0073ae',
