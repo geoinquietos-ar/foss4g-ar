5da5e52b5dbaebfc48e010291272cad0f272f123
Mauricio Miranda <maurimiranda@gmail.com>
2016-02-29 16:52:45 -0300

    Arreglar problemas con js linter

--- app/scripts/programme.js
+++ app/scripts/programme.js
@@ -1,7 +1,9 @@
+'use strict';
+
 $(function() {
   $.getJSON('/programme.json')
     .done(function(data) {
-      var source   = $("#programme-template").html();
+      var source = $('#programme-template').html();
       var template = Handlebars.compile(source);
       $.each(data, function(dayIndex, day) {
         $('#programme').append(template(day));
