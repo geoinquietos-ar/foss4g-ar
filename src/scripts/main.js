5133b9bd87f181e5994e9fac80f487bf8b4571fa
Mauricio Miranda <maurimiranda@gmail.com>
2016-11-02 21:28:37 -0300

    Updates

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import '../styles/main.scss';
-import banner from '../templates/banner.hbs';
-import iso from '../images/iso.png';
-
-document.body.insertAdjacentHTML('beforeend', banner({
-  logo: iso,
-}));
