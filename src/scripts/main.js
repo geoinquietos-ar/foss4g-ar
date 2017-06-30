59f1675a4a4b07f06f2a3e2dee65b9f1474aca54
Cristian Zamar <cristianzamar@gmail.com>
2017-06-30 09:06:29 -0300

    fix secciones

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -84,17 +84,16 @@ L.polygon([
   [-34.57259, -58.43947],
 ]).addTo(map).bindPopup('<b>Instituto Geográfico Nacional</b><br>Avenida Cabildo 301, Palermo, CABA').openPopup();
 
-document.body.insertAdjacentHTML('beforeend', sponsors({
-  logoCarto,
-  logoBoundless,
-}));
 document.body.insertAdjacentHTML('beforeend', speakers({
   avatarMaria,
   avatarVictor,
   avatarJorge,
 }));
 
-document.body.insertAdjacentHTML('beforeend', sponsors());
+document.body.insertAdjacentHTML('beforeend', sponsors({
+  logoCarto,
+  logoBoundless,
+}));
 
 document.body.insertAdjacentHTML('beforeend', faqs());
 document.querySelectorAll('.faqs dt').forEach((element) => {
